2500278b3eba56febc0f113fa8be7219d553635f
Bjoern Vetter <vetterbjoern@yahoo.de>
2026-03-26 20:51:56 +0100

    seo anpassung

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,21 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "RideOut Crew",
-  description: "Biker Community Düsseldorf & Umgebung",
+  title: "RideOut Crew – Motorradgruppe Düsseldorf & Umgebung",
+  description:
+    "RideOut Crew – Deine offene Motorradgruppe aus Düsseldorf. Gemeinsame Ausfahrten für alle Bikes & Fahrstile. Jetzt mitmachen!",
+  keywords:
+    "Motorradgruppe Düsseldorf, Biker Club Düsseldorf, Motorrad Community NRW, Motorradclub Düsseldorf, Bikergruppe Rheinland",
+  authors: [{ name: "RideOut Crew" }],
+  robots: "index, follow",
+  openGraph: {
+    title: "RideOut Crew – Motorradgruppe Düsseldorf",
+    description:
+      "Offene Motorradgruppe aus Düsseldorf. Alle Bikes, alle Fahrstile.",
+    url: "https://rideoutcrew.de",
+    locale: "de_DE",
+    type: "website",
+  },
 };
 
 export default function RootLayout({ children }) {
